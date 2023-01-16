3f1fb6f2d41e6fc53df292b066e137c68ae0355c
zhanyarios004gardi004 <119080464+zhanyarios004gardi004@users.noreply.github.com>
2023-01-16 22:48:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var endpoint = "https://www.jsonstore.io/8ba4fd855086288421f770482e372ccb5a05d906269a34da5884f39eed0418a1";
+var endpoint = "https://zhanyarios004gardi004.github.io/test/8ba4fd855086288421f770482e372ccb5a05d906269a34da5884f39eed0418a1";
 
 function geturl(){
     var url = document.getElementById("urlinput").value;
